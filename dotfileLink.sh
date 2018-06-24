8dd0caa1c7821c1e6c9699666be8f2fd6ba4fca4
hayaosato <jake.bibikyary.880@gmail.com>
2018-06-24 10:27:39 +0900

    fix dotfilesLink

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+# vundle使う
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
 #cd $(dirname $0);
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/utils ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/colors ~/.vim
-
-# vundle使う
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
